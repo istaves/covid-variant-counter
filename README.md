ba3bfd940715074d8fcf0258a5c50e2d00439ab2
istaves <istaves@umich.edu>
2022-08-19 11:41:55 -0700

    external variant dictionary system

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# covid-variant-counter
+# Covid variant counter
+Modified from https://github.com/degregory/Programs/blob/main/NYC_Variant_Counter.py.
+
+Calculates the abundancies of covid variants.
--- var_counter.py
+++ var_counter.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import csv
 
 date = "placeholder"#os.getcwd().split("/")[-1]
 # rbd = receptor-binding domain?
@@ -27,6 +28,16 @@ RBD_variants_dict = {
 "WNYC4"     :   [0, ["Q498Y", "N501T", "F486V", "Y449R"]],
 "WNYC5"     :   [0, ["K417T", "E484A", "Q498", "N440E"]]
 }
+variants_dict = {}
+
+with open("variants_dict.tsv", "r") as variantstsv:
+    variants = csv.reader(variantstsv,delimiter='\t')
+    next(tsvreader)
+    for line in tsvreader:
+        variants_dict[line[0]] = [line[1],line[2:]]
+
+print(variants_dict)
+
 # wastewater treatment facilities
 WWTP_dict = {   1 : [1, "CS", "CS"],
                 2 : [2, "GR", "GR"],
@@ -41,10 +52,6 @@ WWTP_dict2 = {   "CS" : 1,
 
 WWTPs = {}
 Mixed = {}
-OmiPMs = {}
-NTD_WWTPs = {}
-NTD_Mixed = {}
-# NTD_OmiPMs = {}
 
 for file in os.listdir(os.getcwd()): #loops over fileNAMES in current wd
         if (file.lower()).endswith('_chim_rm.tsv') and not 'Collected' in file:
@@ -94,17 +101,17 @@ for file in os.listdir(os.getcwd()): #loops over fileNAMES in current wd
                 else:
                     if not splitline[1] == "Count":
                         matches = []
-                        for variant in RBD_variants_dict:
+                        for variant in variants_dict:
                             check = 0
-                            for SNP in RBD_variants_dict[variant][1]:
-                                try:
-                                    if not SNP in line:
+                            for SNP in variants_dict[variant][1]:
+                                if SNP[0]=="!":
+                                    if SNP[1:] in line:
                                         check += 1
-                                except:
-                                    if SNP[1] in line:
+                                else:
+                                    if SNP not in line:
                                         check += 1
                                     
-                            if check <= RBD_variants_dict[variant][0]:
+                            if check <= variants_dict[variant][0]:
                                 matches.append(variant)
                            
                             
@@ -137,96 +144,6 @@ for file in os.listdir(os.getcwd()): #loops over fileNAMES in current wd
                         
 
             in_file.close()
-        elif (file.lower()).endswith('_deconv.tsv') and not 'Collected' in file:
-            in_file = open(file, "r")
-            if "NY" in file:
-                wwtp = int(file.split("_")[0].strip("NYRBDA"))
-            else:
-                try:
-                    wwtp = WWTP_dict2[file.split("_")[0].strip("0d")]
-                except:
-                    wwtp = WWTP_dict2[file.split("_")[0].strip("0d")[:-1]]
-            try:
-                WWTPs[wwtp]
-            except:
-                WWTPs[wwtp] = {"Alpha" : 0,
-                                "Beta" : 0,
-                                "Gamma" : 0,
-                                "Delta" : 0,
-                                "Kappa" : 0,
-                                "Lambda" : 0,
-                                "Omicron BA.1" : 0,
-                                "Omicron BA.2" : 0,
-                                "Omicron BA.2.12.1" : 0,
-                                "Omicron BA.4/5" : 0,
-                                "L452R" : 0,
-                                "Theta" : 0,
-                                "E484K" : 0,
-                                "S477N" : 0,
-                                "WNYC1" : 0,
-                                "WNYC2" : 0,
-                                "WNYC3" : 0,
-                                "WNYC4" : 0,
-                                "WNYC5" : 0,
-                                "Mixed" : 0,
-                                "Other" : 0
-                                        }
-            try:
-                Mixed[wwtp]
-            except:
-                Mixed[wwtp] = ''
-            
-            for line in in_file:
-                splitline = line.split("\t")
-                try:
-                    splitline[2]
-                except:
-                    pass
-                else:
-                    if not splitline[1] == "Count":
-                        matches = []
-                        for variant in RBD_variants_dict:
-                            check = 0
-                            for SNP in RBD_variants_dict[variant][1]:
-                                try:
-                                    if not SNP in line:
-                                        check += 1
-                                except:
-                                    if SNP[1] in line:
-                                        check += 1
-                                    
-                            if check <= RBD_variants_dict[variant][0]:
-                                matches.append(variant)
-                                
-                                   
-                        if matches:
-                            try:
-                                matches[1]
-                            except:
-                                # print(line + " matches " + " ".join(matches))
-                                try:
-                                    WWTPs[wwtp][matches[0]] += float(splitline[1])
-                                except:
-                                    WWTPs[wwtp][matches[0]] = float(splitline[1])
-                            else:
-                                print(wwtp)
-                                print(line + " matches " + " ".join(matches))
-                                sequence = "'" + line.split("\t")[0] + "'"
-                                if not sequence in Mixed[wwtp]:
-                                    Mixed[wwtp] = Mixed[wwtp] + sequence + ", "
-                                try:
-                                    WWTPs[wwtp]["Mixed"] += float(splitline[1])
-                                except:
-                                    WWTPs[wwtp]["Mixed"] = float(splitline[1])
-
-                        else:
-                            # print(line + " matches " + " none ")
-                            try:
-                                WWTPs[wwtp]["Other"] += float(splitline[1])
-                            except:
-                                WWTPs[wwtp]["Other"] = float(splitline[1])
-
-            in_file.close()
 
 
 outfile = open(date+"RBD_variant_counts.tsv", "w")
@@ -251,44 +168,5 @@ for wwtp in range(1, 5):
         for variant in RBD_variants_dict:
             outfile.write("NR\t ")
     outfile.write("\n")
-outfile.write("\n")
-outfile.write("Code\tWWTP\tBorough\tDate\tOmicron Overlap\n")
-for wwtp in OmiPMs:
-    total = int(sum(WWTPs[wwtp].values()))
-    outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\tOverlap\t% abundance\n")
-    for overlap in OmiPMs[wwtp]:
-        outfile.write(f"\t\t\t\t{overlap}\t{(OmiPMs[wwtp][overlap] / total):.3f}\n")
     
 outfile.close()
-
-# ## NTD out
-# outfile = open(date+"NTD_variant_counts.tsv", "w")
-# outfile.write("Code\tWWTP\tBorough\tDate\tVirus Concentration (virus copies/L)\tNumber of Reads\tFlow rate (to be provided by DEP)\tiSeq/MiSeq\tSRA Accession\tAlpha 203-208Del 429-431Del")
-# outfile.write("\tBeta D80A D215G\tGamma D138Y R190S\tDelta 425A(G142D) 467-472Del(E156G)")
-# outfile.write("\tLambda G75V T76I\tOmicron BA.1\tOmicron BA.2")
-# outfile.write("\tMixed\tOthers\tNTD comments\n")
-
-# for wwtp in range(1, 15):
-    # # print(wwtp)
-    # try:
-        # # print(NTD_WWTPs[wwtp])
-        # total = int(sum(NTD_WWTPs[wwtp].values()))
-        # outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\t\t{total}\t\tMiSeq\t\t")
-        # for variant in NTD_WWTPs[wwtp]:
-            # outfile.write(f"{(NTD_WWTPs[wwtp][variant] / total):.3f}\t ")
-        
-        # outfile.write(NTD_Mixed[wwtp])
-    # except:
-        # outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\t\tNR\t\tMiSeq\t\t")
-        # for variant in NTD_variants_dict:
-            # outfile.write("NR\t ")
-    # outfile.write("\n")
-# # outfile.write("\n")
-# # outfile.write("Code\tWWTP\tBorough\tDate\tOmicron Overlap\n")
-# # for wwtp in NTD_OmiPMs:
-    # # total = int(sum(NTD_WWTPs[wwtp].values()))
-    # # outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\tOverlap\t% abundance\n")
-    # # for overlap in NTD_OmiPMs[wwtp]:
-        # # outfile.write(f"\t\t\t\t{overlap}\t{(NTD_OmiPMs[wwtp][overlap] / total):.3f}\n")
-    
-# outfile.close()
