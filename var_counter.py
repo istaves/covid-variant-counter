aa6b21028f14f080c685224586545df785af39bb
istaves <istaves@umich.edu>
2022-08-19 14:34:40 -0700

    got structure working

--- var_counter.py
+++ var_counter.py
@@ -1,172 +1,111 @@
-    #!/bin/env python3
+#!/bin/env python3
 # modified from Devin Gregory https://github.com/degregory/Programs/blob/main/NYC_Variant_Counter.py
 
 import os
 import sys
 import csv
 
-date = "placeholder"#os.getcwd().split("/")[-1]
-# rbd = receptor-binding domain?
-RBD_variants_dict = {
-"Alpha"     :  [0, ["N501Y", "A570D"]],
-"Beta"      :   [0, ["K417N", "E484K", "N501Y"]],
-"Gamma"     :   [0, ["K417T", "E484K", "N501Y"]],
-"Delta"     :   [0, [["not", "K417N"], ["not", "N440K"], ["not", "S466N"], "L452R", "T478K", ]],
-"Kappa"     :   [0, ["L452R", "E484Q"]],
-"Lambda"    :   [0, ["L452Q", "F590S"]],
-"Omicron BA.1"   :   [1, ["1251T(K417N)", "1320G(N440K)", "1336A(G446S)", ["not", "L452Q"], "1430A(S477N)", "1433A(T478K)", "1451C(E484A)", "1478G(Q493R)", "1486A(G496S)", "1493G(Q498R)", "1501T(N501Y)", "1513C(Y505H)", "1640A(T547K)"]],
-"Omicron BA.2"   :   [0, ["1251T(K417N)", "1320G(N440K)", ["not", "G446"],["not", "L452Q"],  "1430A(S477N)", "1433A(T478K)", "1451C(E484A)", "1478G(Q493R)", ["not", "G496"], "1493G(Q498R)", "1501T(N501Y)", "1513C(Y505H)", ["not", "T547"]]],
-"Omicron BA.2.12.1"   :   [0, ["1251T(K417N)", "1320G(N440K)", ["not", "G446"], "1355A(L452Q)", "1430A(S477N)", "1433A(T478K)", "1451C(E484A)", "1478G(Q493R)", ["not", "G496"], "1493G(Q498R)", "1501T(N501Y)", "1513C(Y505H)", ["not", "T547"]]],
-"Omicron BA.4/5"   :   [0, ["1251T(K417N)", "1320G(N440K)", ["not", "G446"], "1355G(L452R)", "1430A(S477N)", "1433A(T478K)", "1451C(E484A)", "T1456G(F486V)", ["not", "1478G(Q493R)"], ["not", "G496"], "1493G(Q498R)", "1501T(N501Y)", "1513C(Y505H)", ["not", "T547"]]],
-"L452R"     :   [0, ["L452R", ["not", "T478K"], ["not", "E484Q"], ["not", "Q498"]]],
-"Theta/Mu"     :   [0, ["E484K", "N501Y", ["not", "K417"]]],
-"E484K"     :   [0, ["E484K", ["not", "N501Y"], ["not", "K417"]]],
-"S477N"     :   [0, ["S477N", ["not", "N501Y"], ["not", "K417"], ["not", "T478K"]]],
-"WNYC1"     :   [1, ["Q498Y", "H519N", "H519K", "E484A", "S494P", "T572N"]],
-"WNYC2"     :   [0, ["Q498Y", "H519N", "Q493K"]],
-"WNYC3"     :   [0, ["K417T", "E484A", "Q498", "K444T"]],
-"WNYC4"     :   [0, ["Q498Y", "N501T", "F486V", "Y449R"]],
-"WNYC5"     :   [0, ["K417T", "E484A", "Q498", "N440E"]]
-}
+print("Variant Counter")
 variants_dict = {}
 
-with open("variants_dict.tsv", "r") as variantstsv:
-    variants = csv.reader(variantstsv,delimiter='\t')
-    next(tsvreader)
-    for line in tsvreader:
-        variants_dict[line[0]] = [line[1],line[2].split(',')]
-
-print(variants_dict)
+try:
+    with open("dictionary.csv", "r") as dictcsv:
+        variants = csv.reader(dictcsv)
+        next(variants)
+        for line in variants:
+            variants_dict[line[0]] = [int(line[1]),line[2].split()]
+    print("- Successfully read variant dictionary.") 
+except IOError as error:
+    print("- Error reading variant dictionary!")
+# print(variants_dict)
 
 # wastewater treatment facilities
-WWTP_dict = {   1 : [1, "CS", "CS"],
-                2 : [2, "GR", "GR"],
-                3 : [3, "V1", "V1"],
-                4 : [4, "WB", "WB"]
-}
-WWTP_dict2 = {   "CS" : 1,
-                "GR" : 2,
-                "V1" : 3,
-                "WB" : 4
-}
+
 
 WWTPs = {}
 Mixed = {}
 
-for file in os.listdir(os.getcwd()): #loops over fileNAMES in current wd
-        if (file.lower()).endswith('_chim_rm.tsv') and not 'Collected' in file:
-            in_file = open(file, "r")
-            if "NY" in file:
-                wwtp = int(file.split("_")[0].strip("NYRBDA"))
-            else: 
-                try:
-                    wwtp = WWTP_dict2[file.split("_")[0].strip("0d")]
-                except:
-                    wwtp = WWTP_dict2[file.split("_")[0].strip("0d")[:-1]]
-            try:
-                WWTPs[wwtp]
-            except:
-                WWTPs[wwtp] = {"Alpha" : 0,
-                                "Beta" : 0,
-                                "Gamma" : 0,
-                                "Delta" : 0,
-                                "Kappa" : 0,
-                                "Lambda" : 0,
-                                "Omicron BA.1" : 0,
-                                "Omicron BA.2" : 0,
-                                "Omicron BA.2.12.1" : 0,
-                                "Omicron BA.4/5" : 0,
-                                "L452R" : 0,
-                                "Theta/Mu" : 0,
-                                "E484K" : 0,
-                                "S477N" : 0,
-                                "WNYC1" : 0,
-                                "WNYC2" : 0,
-                                "WNYC3" : 0,
-                                "WNYC4" : 0,
-                                "WNYC5" : 0,
-                                "Mixed" : 0,
-                                "Other" : 0
-                                        }
-            try:
-                Mixed[wwtp]
-            except:
-                Mixed[wwtp] = ''
-            for line in in_file:
-                splitline = line.split("\t")
-                try:
-                    splitline[2]
-                except:
-                    pass
+files = [file for file in os.listdir(os.getcwd()) if (file.lower()).endswith('chim_rm.tsv') and not 'Collected' in file]
+indexedfiles = {}
+dates = [*set([file[2:8] for file in files])]
+dates.sort()
+for date in dates:
+    indexedfiles[date] = [file for file in files if file[2:8]==date]
+
+print("- Found data on:")
+for date in dates:
+    print(f"   {date[:2]}.{date[2:4]}.{date[4:]} from WWTPs {', '.join([file[:2] for file in indexedfiles[date]])}")
+
+print("- Successfully wrote files:")
+for date in dates:
+    for file in indexedfiles[date]: #loops over fileNAMES in current wd
+        in_file = open(file, "r")
+        wwtp = file[:2]
+        try:
+            WWTPs[wwtp]
+        except:
+            WWTPs[wwtp] = {}
+            for variant in variants_dict:
+                WWTPs[wwtp][variant] = 0
+        try:
+            Mixed[wwtp]
+        except:
+            Mixed[wwtp] = ''
+        for line in in_file:
+
+            reader = csv.reader(in_file,delimiter="\t")
+            next(reader)
+            next(reader)
+            for line in reader:
+                matches = []
+                for variant in variants_dict:
+                    check = 0
+                    for SNP in variants_dict[variant][1]:
+                        if SNP[0]=="!" and SNP[1:] in line[0]:
+                            check += 1
+                        elif SNP not in line[0]:
+                            check += 1
+                        if check <= variants_dict[variant][0]:
+                            matches.append(variant)
+                if len(matches) > 1:
+                    #print(wwtp)
+                    #print(line + " matches " + " ".join(matches))
+                    if not "'" + line[0] + "'" in Mixed[wwtp]:
+                        Mixed[wwtp] = "'" + line[0] + "', "
+                    try:
+                        WWTPs[wwtp]["Mixed"] += float(line[1])
+                    except:
+                        WWTPs[wwtp]["Mixed"] = float(line[1])
+                elif matches:
+                    try:
+                        WWTPs[wwtp][matches[0]] += float(line[1])
+                    except:
+                        WWTPs[wwtp][matches[0]] = float(line[1])
                 else:
-                    if not splitline[1] == "Count":
-                        matches = []
-                        for variant in variants_dict:
-                            check = 0
-                            for SNP in variants_dict[variant][1]:
-                                if SNP[0]=="!":
-                                    if SNP[1:] in line:
-                                        check += 1
-                                else:
-                                    if SNP not in line:
-                                        check += 1
-                                    
-                            if check <= variants_dict[variant][0]:
-                                matches.append(variant)
-                           
-                            
-                        if matches:
-                            try:
-                                matches[1]
-                            except:
-                                # print(line + " matches " + " ".join(matches))
-                                try:
-                                    WWTPs[wwtp][matches[0]] += float(splitline[1])
-                                except:
-                                    WWTPs[wwtp][matches[0]] = float(splitline[1])
-                            else:
-                                print(wwtp)
-                                print(line + " matches " + " ".join(matches))
-                                sequence = "'" + line.split("\t")[0] + "'"
-                                if not sequence in Mixed[wwtp]:
-                                    Mixed[wwtp] = Mixed[wwtp] + sequence + ", "
-                                try:
-                                    WWTPs[wwtp]["Mixed"] += float(splitline[1])
-                                except:
-                                    WWTPs[wwtp]["Mixed"] = float(splitline[1])
-
-                        else:
-                            # print(line + " matches " + " none ")
-                            try:
-                                WWTPs[wwtp]["Other"] += float(splitline[1])
-                            except:
-                                WWTPs[wwtp]["Other"] = float(splitline[1])
+                    # print(line + " matches " + " none ")
+                    try:
+                        WWTPs[wwtp]["Other"] += float(line[1])
+                    except:
+                        WWTPs[wwtp]["Other"] = float(line[1])
                         
+        in_file.close()
 
-            in_file.close()
 
+    outfile = open(date+"_variant_counts.tsv", "w")
+    outfile.write("WWTP\tDate\tVirus Concentration (virus copies/L)\tNumber of Reads\tFlow rate (to be provided by DEP)\tiSeq/MiSeq\tSRA Accession\tAlpha N501Y+A570D")
+    outfile.write("\tBeta K417N+E484K+N501Y\tGamma K417T+E484K+N501Y\tDelta L452R+T478K")
+    outfile.write("\tKappa L452R+E484Q\tLambda L452R+F590S\tOmicron BA.1\tOmicron BA.2\tOmicron BA.2.12.1\tOmicron BA.4/5")
+    outfile.write("\tL452R (not T478K E484Q or Q498)\tTheta/Mu E484K N501Y (not K417)\tE484K (not K417 or N501Y)\tS477N (not K417 T478K or N501Y)")
+    outfile.write("\tWNY1 Family Q498Y H519 E484A\tWNY2 Family Q498Y H519N Q493K\tWNY3 Family K417T E484A Q498 K444T\tWNY4 Family Q498Y N501T F486V Y449R\tWNY5 Family K417T E484A Q498 N440E\tMixed\tOthers\tRBD comments\n")
 
-outfile = open(date+"RBD_variant_counts.tsv", "w")
-outfile.write("Code\tWWTP\tBorough\tDate\tVirus Concentration (virus copies/L)\tNumber of Reads\tFlow rate (to be provided by DEP)\tiSeq/MiSeq\tSRA Accession\tAlpha N501Y+A570D")
-outfile.write("\tBeta K417N+E484K+N501Y\tGamma K417T+E484K+N501Y\tDelta L452R+T478K")
-outfile.write("\tKappa L452R+E484Q\tLambda L452R+F590S\tOmicron BA.1\tOmicron BA.2\tOmicron BA.2.12.1\tOmicron BA.4/5")
-outfile.write("\tL452R (not T478K E484Q or Q498)\tTheta/Mu E484K N501Y (not K417)\tE484K (not K417 or N501Y)\tS477N (not K417 T478K or N501Y)")
-outfile.write("\tWNY1 Family Q498Y H519 E484A\tWNY2 Family Q498Y H519N Q493K\tWNY3 Family K417T E484A Q498 K444T\tWNY4 Family Q498Y N501T F486V Y449R\tWNY5 Family K417T E484A Q498 N440E\tMixed\tOthers\tRBD comments\n")
-
-for wwtp in range(1, 5):
-    # print(wwtp)
-    try:
-        # print(WWTPs[wwtp])
+    for wwtp in WWTPs:
         total = int(sum(WWTPs[wwtp].values()))
-        outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\t\t{total}\t\tMiSeq\t\t")
+        outfile.write(f"{wwtp}\t{date}\t\t{total}\t\tMiSeq\t\t")
         for variant in WWTPs[wwtp]:
             outfile.write(f"{(WWTPs[wwtp][variant] / total):.3f}\t ")
         
         outfile.write(Mixed[wwtp])
-    except:
-        outfile.write(f"{WWTP_dict[int(wwtp)][0]}\t{WWTP_dict[int(wwtp)][1]}\t{WWTP_dict[int(wwtp)][2]}\t{date}\t\tNR\t\tMiSeq\t\t")
-        for variant in RBD_variants_dict:
-            outfile.write("NR\t ")
-    outfile.write("\n")
+        outfile.write("\n")
     
-outfile.close()
+    outfile.close()
+    print("   "+date+"_variant_counts.tsv")
