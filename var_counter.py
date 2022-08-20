54af42f1ff9258f9730d07787afc0fc2acd211ec
istaves <istaves@umich.edu>
2022-08-20 14:36:19 -0700

    fixed bugs and set to output uncharacterized sequences

--- var_counter.py
+++ var_counter.py
@@ -6,9 +6,9 @@ import sys
 import csv
 print("Variant Counter")
 variants_dict = {}
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
 try:
-    with open("dictionary.csv", "r") as dictcsv:
+    with open(dir_path + "\\dictionary.csv", "r") as dictcsv:
         variants = csv.reader(dictcsv)
         next(variants)
         for line in variants:
@@ -24,6 +24,7 @@ if path[-1] != '/':
 
 WWTPs = {}
 Mixed = {}
+Other = {}
 
 files = [file for file in os.listdir(path) if ((file.lower()).endswith('chim_rm.tsv') or (file.lower()).endswith('covar_deconv.tsv')) and not 'Collected' in file]
 indexedfiles = {}
@@ -34,7 +35,7 @@ for date in dates:
 
 print("- Found data on:")
 for date in dates:
-    print(f"   {date[:2]}.{date[2:4]}.{date[4:]} from WWTPs {', '.join([*set([file[:2] for file in indexedfiles[date]])])}")
+    print(f"   {date[:2]}.{date[2:4]}.{date[4:]} from {', '.join([*set([file[:2] for file in indexedfiles[date]])])}")
 
 print("- Successfully wrote files:")
 for date in dates:
@@ -47,10 +48,16 @@ for date in dates:
             WWTPs[wwtp] = {}
             for variant in variants_dict:
                 WWTPs[wwtp][variant] = 0
+            WWTPs[wwtp]["Mixed"] = 0
+            WWTPs[wwtp]["Other"] = 0
         try:
             Mixed[wwtp]
         except:
             Mixed[wwtp] = ''
+        try:
+            Other[wwtp]
+        except:
+            Other[wwtp] = ''
         reader = csv.reader(in_file,delimiter="\t")
         next(reader)
         next(reader)
@@ -68,7 +75,7 @@ for date in dates:
                     matches.append(variant)
             if len(matches) > 1:
                 if not "'" + line[0] + "'" in Mixed[wwtp]:
-                    Mixed[wwtp] = "'" + line[0] + "', "
+                    Mixed[wwtp] += "'" + line[0] + "', "
                 try:
                     WWTPs[wwtp]["Mixed"] += float(line[1])
                 except:
@@ -79,29 +86,26 @@ for date in dates:
                 except:
                     WWTPs[wwtp][matches[0]] = float(line[1])
             else:
+                if not "'" + line[0] + "'" in Other[wwtp]:
+                    Other[wwtp] += "'" + line[0] + "', "
                 try:
                     WWTPs[wwtp]["Other"] += float(line[1])
                 except:
                     WWTPs[wwtp]["Other"] = float(line[1])
-
         in_file.close()
 
-
     outfile = open(path+date+"_variant_counts.tsv", "w")
-    outfile.write("WWTP\tDate\tVirus Concentration (virus copies/L)\tNumber of Reads\tFlow rate (to be provided by DEP)\tiSeq/MiSeq\tSRA Accession\tAlpha N501Y+A570D")
-    outfile.write("\tBeta K417N+E484K+N501Y\tGamma K417T+E484K+N501Y\tDelta L452R+T478K")
-    outfile.write("\tKappa L452R+E484Q\tLambda L452R+F590S\tOmicron BA.1\tOmicron BA.2\tOmicron BA.2.12.1\tOmicron BA.4/5")
-    outfile.write("\tL452R (not T478K E484Q or Q498)\tTheta/Mu E484K N501Y (not K417)\tE484K (not K417 or N501Y)\tS477N (not K417 T478K or N501Y)")
-    outfile.write("\tWNY1 Family Q498Y H519 E484A\tWNY2 Family Q498Y H519N Q493K\tWNY3 Family K417T E484A Q498 K444T\tWNY4 Family Q498Y N501T F486V Y449R\tWNY5 Family K417T E484A Q498 N440E\tMixed\tOthers\tRBD comments\n")
-
+    outfile.write("Code\tDate\tVirus Concentration (virus copies/L)\tNumber of Reads\tFlow rate (to be provided by DEP)\tSRA Accession")
+    for variant in variants_dict:
+        outfile.write("\t" + variant)
+    outfile.write("\tMixed\tMixed sequences\tOther\tOther sequences\tComments\n")
     for wwtp in WWTPs:
         total = int(sum(WWTPs[wwtp].values()))
-        outfile.write(f"{wwtp}\t{date}\t\t{total}\t\tMiSeq\t\t")
-        for variant in WWTPs[wwtp]:
+        outfile.write(f"{wwtp}\t{date}\t\t{total}\t\t\t")
+        for variant in variants_dict:
             outfile.write(f"{(WWTPs[wwtp][variant] / total):.3f}\t ")
-
-        outfile.write(Mixed[wwtp])
-        outfile.write("\n")
+        outfile.write(f"{(WWTPs[wwtp]['Mixed'] / total):.3f}\t{Mixed[wwtp]}\t")
+        outfile.write(f"{(WWTPs[wwtp]['Other'] / total):.3f}\t{Other[wwtp]}\n")
 
     outfile.close()
     print("   "+date+"_variant_counts.tsv")
