753409a7a8e58582971efea7ab68f63c1a4a34e0
Ian Staves <istaves@DESKTOP-23E1VMP.attlocal.net>
2022-08-22 10:49:32 -0400

    updated program and readme

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
 # Covid variant counter
-Modified from https://github.com/degregory/Programs/blob/main/NYC_Variant_Counter.py.
 
-Calculates the abundancies of covid variants.
+Modified from
+[this script](https://github.com/degregory/Programs/blob/main/NYC_Variant_Counter.py).
 
-## Usage 
-Download [the python script](https://github.com/istaves/covid-variant-counter/releases/download/release/var_counter.py) and [the sample dictionary](https://github.com/istaves/covid-variant-counter/releases/download/release/dictionary.csv) (or modify/create your own) and put them in the same directory.
-Run var_counter.py. The program will ask for the directory that you'd like the program to work on. It will put its output files into that directory as well.
+## Setup
+
+1. Be sure `python` is installed.
+2. Download
+   [var_counter.py](https://github.com/istaves/covid-variant-counter/releases/download/release/var_counter.py).
+3. Be sure a file with information about the variants you'd like to search for,
+   called `dictionary.csv` (see below), is in the same directory as the python
+   script. An example is provided here:
+   [dictionary.csv](https://github.com/istaves/covid-variant-counter/releases/download/release/dictionary.csv)
+
+and
+[the sample dictionary](https://github.com/istaves/covid-variant-counter/releases/download/release/dictionary.csv)
+(or modify/create your own) and put them in the same directory. Run
+`var_counter.py`. The program will ask for the directory that you'd like the
+program to work on. It will put its output files into that directory as well.
 
 ## Variant dictionary
-The dictionary can be as long or as short as desired, depending on the number of variants that should be searched for. The first field on each line is the variant name. The second field is the tolerance of the search (i.e., a tolerance of 0 means that every SNP must match in order to classify as that variant, a tolerance of 1 means that 1 SNP can be missing, etc.). The third field is a list of SNPs to search for separated by spaces (' '). Any SNP that should NOT be present in the variant should be prepended with an exclamation point ('!').
+
+The dictionary can be as long or as short as desired, depending on the number of
+variants that should be searched for. The first field on each line is the
+variant name. The second field is the tolerance of the search (i.e., a tolerance
+of 0 means that every SNP must match in order to classify as that variant, a
+tolerance of 1 means that 1 SNP can be missing, etc.). The third field is a list
+of SNPs to search for separated by spaces (' '). Any SNP that should NOT be
+present in the variant should be prepended with an exclamation point ('!').
--- var_counter.py
+++ var_counter.py
@@ -2,8 +2,8 @@
 # modified from Devin Gregory https://github.com/degregory/Programs/blob/main/NYC_Variant_Counter.py
 
 import os
-import sys
 import csv
+
 print("Variant Counter")
 variants_dict = {}
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -12,30 +12,39 @@ try:
         variants = csv.reader(dictcsv)
         next(variants)
         for line in variants:
-            variants_dict[line[0]] = [int(line[1]),line[2].split()]
-    print("- Successfully read variant dictionary.") 
+            variants_dict[line[0]] = [int(line[1]), line[2].split()]
+    print("- Successfully read variant dictionary.")
 except IOError as error:
     print("- Error reading variant dictionary!")
-# print(variants_dict)
 
-path = input("- Enter path to files: ").replace("\\","/")
-if path[-1] != '/':
-    path += '/'
+path = input("- Enter path to files: ").replace("\\", "/")
+if path[-1] != "/":
+    path += "/"
 
 WWTPs = {}
 Mixed = {}
 Other = {}
 
-files = [file for file in os.listdir(path) if ((file.lower()).endswith('chim_rm.tsv') or (file.lower()).endswith('covar_deconv.tsv')) and not 'Collected' in file]
+files = [
+    file
+    for file in os.listdir(path)
+    if (
+        (file.lower()).endswith("chim_rm.tsv")
+        or (file.lower()).endswith("covar_deconv.tsv")
+    )
+    and not "Collected" in file
+]
 indexedfiles = {}
 dates = [*set([file[2:8] for file in files])]
 dates.sort()
 for date in dates:
-    indexedfiles[date] = [file for file in files if file[2:8]==date]
+    indexedfiles[date] = [file for file in files if file[2:8] == date]
 
 print("- Found data on:")
 for date in dates:
-    print(f"   {date[:2]}.{date[2:4]}.{date[4:]} from {', '.join([*set([file[:2] for file in indexedfiles[date]])])}")
+    print(
+        f"   {date[:2]}.{date[2:4]}.{date[4:]} from {', '.join([*set([file[:2] for file in indexedfiles[date]])])}"
+    )
 
 print("- Successfully wrote files:")
 for date in dates:
@@ -53,12 +62,12 @@ for date in dates:
         try:
             Mixed[wwtp]
         except:
-            Mixed[wwtp] = ''
+            Mixed[wwtp] = ""
         try:
             Other[wwtp]
         except:
-            Other[wwtp] = ''
-        reader = csv.reader(in_file,delimiter="\t")
+            Other[wwtp] = ""
+        reader = csv.reader(in_file, delimiter="\t")
         next(reader)
         next(reader)
         for line in reader:
@@ -66,7 +75,7 @@ for date in dates:
             for variant in variants_dict:
                 check = 0
                 for SNP in variants_dict[variant][1]:
-                    if SNP[0]=="!":
+                    if SNP[0] == "!":
                         if SNP[1:] in line[0]:
                             check += 1
                     elif SNP not in line[0]:
@@ -94,7 +103,7 @@ for date in dates:
                     WWTPs[wwtp]["Other"] = float(line[1])
         in_file.close()
 
-    outfile = open(path+date+"_variant_counts.tsv", "w")
+    outfile = open(path + date + "_variant_counts.tsv", "w")
     outfile.write("Code\tDate\tNumber of Reads")
     for variant in variants_dict:
         outfile.write("\t" + variant)
@@ -108,7 +117,7 @@ for date in dates:
         outfile.write(f"{(WWTPs[wwtp]['Other'] / total):.3f}\t{Other[wwtp]}\n")
 
     outfile.close()
-    print("   "+date+"_variant_counts.tsv")
+    print("   " + date + "_variant_counts.tsv")
 
 
 input("Press ENTER to exit...")
