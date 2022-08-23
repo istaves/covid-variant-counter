c974e82d41359e69d5554268c54e689513058f99
Ian Staves <istaves@umich.edu>
2022-08-22 20:02:44 -0400

    changed output to 1 file and rewrote

--- var_counter.py
+++ var_counter.py
@@ -8,7 +8,7 @@ import csv
 ### READ IN VARIANT DICTIONARY ###
 print("Variant Counter")
 variants_dict = {}
-dir_path = os.path.dirname(os.path.realpath(__file__)).replace("\\","/")
+dir_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 try:
     with open(dir_path + "/dictionary.csv", "r") as dictcsv:
         variants = csv.reader(dictcsv)
@@ -24,9 +24,12 @@ path = input("- Enter path to files: ").replace("\\", "/")
 if path[-1] != "/":
     path += "/"
 
-WWTPs = {}
-Mixed = {}
-Other = {}
+rows = {}
+header = (
+    ["Code", "Date", "Number of Reads"]
+    + [var for var in variants_dict]
+    + ["Mixed", "Mixed variants", "Other", "Other variants"]
+)
 
 ### GET LIST OF VALID INPUT FILES ###
 files = [
@@ -39,93 +42,66 @@ files = [
     and not "Collected" in file
 ]
 
-### PARSE METADATA FROM FILENAMES ###
-indexedfiles = {}
-dates = [*set([file[2:8] for file in files])]
-dates.sort()
-for date in dates:
-    indexedfiles[date] = [file for file in files if file[2:8] == date]
+ids = sorted([*set([f[:8] for f in files])])
+for i in ids:
+    rows[i] = {}
+    rows[i]["Code"] = i[:2]
+    rows[i]["Date"] = i[2:8]
+    rows[i]["Number of Reads"] = 0
+    for var in variants_dict:
+        rows[i][var] = 0
+    rows[i]["Mixed"] = 0
+    rows[i]["Mixed variants"] = ""
+    rows[i]["Other"] = 0
+    rows[i]["Other variants"] = ""
 
 print("- Found data on:")
-for date in dates:
+for date in sorted([*set([i[2:8] for i in ids])]):
     print(
-        f"   {date[:2]}.{date[2:4]}.{date[4:]} from {', '.join([*set([file[:2] for file in indexedfiles[date]])])}"
+            f"    {date[:2]}.{date[2:4]}.{date[4:]} from {', '.join([i[:2] for i in ids if i[2:] == date])}"
     )
 
-print("- Successfully wrote files:")
-for date in dates: # loop to create seperate output for each date
-    ### PROCESS DATA ###
-    for file in indexedfiles[date]:
-        in_file = open(path + file, "r")
-        wwtp = file[:2] # initialize data from this WWTP
-        try:
-            WWTPs[wwtp]
-        except:
-            WWTPs[wwtp] = {}
-            for variant in variants_dict:
-                WWTPs[wwtp][variant] = 0
-            WWTPs[wwtp]["Mixed"] = 0
-            WWTPs[wwtp]["Other"] = 0
-        try:
-            Mixed[wwtp]
-        except:
-            Mixed[wwtp] = ""
-        try:
-            Other[wwtp]
-        except:
-            Other[wwtp] = ""
-        reader = csv.reader(in_file, delimiter="\t")
-        next(reader)
-        next(reader)
-        for line in reader: # search for matches
-            matches = []
-            for variant in variants_dict:
-                check = 0
-                for SNP in variants_dict[variant][1]:
-                    if SNP[0] == "!":
-                        if SNP[1:] in line[0]:
-                            check += 1
-                    elif SNP not in line[0]:
+### PROCESS DATA ###
+for file in files:
+    in_file = open(path + file, "r")
+    id = file[:8]
+    reader = csv.reader(in_file, delimiter="\t")
+    next(reader)
+    next(reader)
+    for line in reader:  # search for matches
+        matches = []
+        for var in variants_dict:
+            check = 0
+            for SNP in variants_dict[var][1]:
+                if SNP[0] == "!":
+                    if SNP[1:] in line[0]:
                         check += 1
-                if check <= variants_dict[variant][0]:
-                    matches.append(variant)
-            if len(matches) > 1: # if matches multiple variants, classify as "mixed" and record sequence data
-                if not "'" + line[0] + "'" in Mixed[wwtp]:
-                    Mixed[wwtp] += "'" + line[0] + "', "
-                try:
-                    WWTPs[wwtp]["Mixed"] += float(line[1])
-                except:
-                    WWTPs[wwtp]["Mixed"] = float(line[1])
-            elif matches: # if matches one variant, classify accordingly
-                try:
-                    WWTPs[wwtp][matches[0]] += float(line[1])
-                except:
-                    WWTPs[wwtp][matches[0]] = float(line[1])
-            else: # if matches no variants, classify as "other" and record sequence data
-                if not "'" + line[0] + "'" in Other[wwtp]:
-                    Other[wwtp] += "'" + line[0] + "', "
-                try:
-                    WWTPs[wwtp]["Other"] += float(line[1])
-                except:
-                    WWTPs[wwtp]["Other"] = float(line[1])
-        in_file.close()
+                elif SNP not in line[0]:
+                    check += 1
+            if check <= variants_dict[var][0]:
+                matches.append(var)
+        if len(matches) > 1:  # if matches multiple variants
+            rows[id]["Mixed"] += float(line[1])
+            if not "'" + line[0] + "'" in rows[id]["Mixed variants"]:
+                rows[id]["Mixed variants"] += "'" + line[0] + "', "
+        elif matches:  # if matches one variant, classify accordingly
+            rows[id][matches[0]] += float(line[1])
+        else:  # if matches no variants, classify as "other" and record sequence data
+            rows[id]["Other"] += float(line[1])
+            if not "'" + line[0] + "'" in rows[id]["Other variants"]:
+                rows[id]["Other variants"] += "'" + line[0] + "', "
+        rows[id]["Number of Reads"] += float(line[1])
+    in_file.close()
 
-    ### WRITE OUTPUT FILES ###
-    outfile = open(path + date + "_variant_counts.tsv", "w")
-    outfile.write("Code\tDate\tNumber of Reads")
-    for variant in variants_dict:
-        outfile.write("\t" + variant)
-    outfile.write("\tMixed\tMixed variants\tOther\tOther variants\tComments\n")
-    for wwtp in WWTPs:
-        total = int(sum(WWTPs[wwtp].values()))
-        outfile.write(f"{wwtp}\t{date}\t{total}\t")
-        for variant in variants_dict:
-            outfile.write(f"{(WWTPs[wwtp][variant] / total):.3f}\t ")
-        outfile.write(f"{(WWTPs[wwtp]['Mixed'] / total):.3f}\t{Mixed[wwtp]}\t")
-        outfile.write(f"{(WWTPs[wwtp]['Other'] / total):.3f}\t{Other[wwtp]}\n")
+### WRITE OUTPUT FILES ###
+with open(path + "variant_counts.tsv", "w") as f:
+    writer = csv.DictWriter(f, fieldnames=header, delimiter="\t")
+    writer.writeheader()
+    for id in rows:
+        for var in [i for i in variants_dict] + ["Mixed", "Other"]:
+            rows[id][var] = round((rows[id][var] / rows[id]["Number of Reads"]), 3)
+        writer.writerow(rows[id])
 
-    outfile.close()
-    print("   " + date + "_variant_counts.tsv")
-
+print("- Successfully wrote file variant_counts.tsv")
 
 input("Press ENTER to exit...")
